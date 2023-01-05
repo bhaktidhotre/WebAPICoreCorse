cce8ac43a9d8c915ed73ecede86b65e3bf94650a
bhaktidhotre <bhaktidhotre185@gmail.com>
2023-01-05 22:36:05 +0530

    Migration Added

---
+++ NZWalkAPI/NZWalkAPI/Data/NZWalkDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalkAPI.Model.Domain;
+
+namespace NZWalkAPI.Data
+{
+    public class NZWalkDbContext : DbContext
+    {
+        public NZWalkDbContext(DbContextOptions<NZWalkDbContext> options):base(options) { }
+       
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalksDifficulty { get; set; }  
+    }
+}
---
+++ NZWalkAPI/NZWalkAPI/Model/Domain/Walk.cs
@@ -0,0 +1,13 @@
+﻿namespace NZWalkAPI.Model.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Lenght { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkdifficultyId { get; set; }
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
